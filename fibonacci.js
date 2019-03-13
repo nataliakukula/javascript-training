61d0c5a6d074e108741b5e9097235e906c7541ed
Kinky Cookie <kuki_kukula@yahoo.com>
2019-03-13 16:10:46 -0500

    fibonacci examples

---
+++ fibonacci.js
@@ -0,0 +1,24 @@
+// 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, ...
+// based on the "index" number return the fib number
+
+function fibonacci(num) {
+    if (num <= 1) return 1;
+
+    return fibonacci(num - 1) + fibonacci(num - 2);
+}
+
+console.log(fibonacci(5));
+
+
+function fibonacci(num) {
+    var a = 1, b = 0, temp;
+
+    while (num >= 0) {
+        temp = a;
+        a = a + b;
+        b = temp;
+        num--;
+    }
+
+    return b;
+}
